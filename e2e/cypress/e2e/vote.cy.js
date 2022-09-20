ca5d5e27e58076384497de895c2f3066b66be0db
Jayd Page <jayd.page@chillisoft.co.za>
2022-09-20 19:54:40 +0200

    Add test to validate that features are sorted by their score

--- e2e/cypress/e2e/vote.cy.js
+++ e2e/cypress/e2e/vote.cy.js
@@ -33,4 +33,38 @@ describe('voting', () => {
       cy.getByAttribute('feature-score').hasText('3')
     })
   })
+
+  it('features are sorted by score descending', () => {
+    // Arrange
+    const feature1 = 'feature 1'
+    const feature2 = 'feature 2'
+    const feature3 = 'feature 3'
+    cy.createRequest(feature1)
+    cy.createRequest(feature2)
+    cy.createRequest(feature3)
+    cy.visitHomePage()
+    cy.getFeatures().then((features) => {
+      const id2 = features.find((x) => x.title === feature2).id
+      const id3 = features.find((x) => x.title === feature3).id
+      // Act
+      cy.getByAttribute('request-item-0').hasText(feature1)
+      cy.getByAttribute('request-item-1').hasText(feature2)
+      cy.getByAttribute('request-item-2').hasText(feature3)
+
+      cy.vote({ id: id3, title: feature3, ipAddress: '192.168.1.1' })
+      cy.visitHomePage()
+      // Assert
+      cy.getByAttribute('request-item-0').hasText(feature3)
+      cy.getByAttribute('request-item-1').hasText(feature1)
+      cy.getByAttribute('request-item-2').hasText(feature2)
+
+      cy.vote({ id: id2, title: feature2, ipAddress: '192.168.1.1' })
+      cy.vote({ id: id2, title: feature2, ipAddress: '192.168.1.2' })
+      cy.visitHomePage()
+
+      cy.getByAttribute('request-item-0').hasText(feature2)
+      cy.getByAttribute('request-item-1').hasText(feature3)
+      cy.getByAttribute('request-item-2').hasText(feature1)
+    })
+  })
 })
